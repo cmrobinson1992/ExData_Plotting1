5202d90934584b3dfc8f526d5200419e8f7eebe0
cmrobinson1992 <christian.m.robinson@gmail.com>
2017-10-22 21:13:12 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ getwd()
 #create subsetted dataset with only data from 2/1/2007 and 2/2/2007
 > data <- "household_power_consumption.txt"
 > dataset <- read.table(data, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
-> dataset <- dataset[(dataset$Date %in% c("2/1/2007", "2/2/2007")), ]
+> dataset <- dataset[(dataset$Date %in% c("1/2/2007", "2/2/2007")), ]
 
 #create histogram of global active power
 > global_active_power <- as.numeric(dataset$Global_active_power)
